abc063f1c973078f37b9c1df141af0b762fcd42d
Stepan_Penyaz <Stepan_Penyaz@epam.com>
2022-05-09 10:24:44 +0400

    Initial: Added console and test projects, readme file with first kata.

---
+++ Unit_7-Kata_1-Odd-Even.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit_7_Kata_1_Odd_Even.ConsoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Unit_7-Kata_1-Odd-Even.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Unit_7_Kata_1_Odd_Even.Tests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}

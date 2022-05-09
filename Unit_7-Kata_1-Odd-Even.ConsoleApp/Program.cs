a3d4537fdc17b71b101c0a9104a65b37ca5ab485
Stepan_Penyaz <Stepan_Penyaz@epam.com>
2022-05-09 15:16:27 +0400

    Feature: Added console UI implementation.

--- Unit_7-Kata_1-Odd-Even.ConsoleApp/Program.cs
+++ Unit_7-Kata_1-Odd-Even.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unit_7_Kata_1_Odd_Even.NumberProcessing;
 
 namespace Unit_7_Kata_1_Odd_Even.ConsoleApp
 {
@@ -6,7 +7,52 @@ namespace Unit_7_Kata_1_Odd_Even.ConsoleApp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+
+      int startIndex = 0, endIndex = 0;
+
+      while (true)
+      {
+        Console.WriteLine("Please enter a start index");
+        try
+        {
+          startIndex = int.Parse(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+          Console.WriteLine("Incorrect value");
+          continue;
+        }
+        Console.WriteLine("Please enter a end index");
+        try
+        {
+          endIndex = int.Parse(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+          Console.WriteLine("Incorrect value");
+          continue;
+        }
+        if (startIndex > endIndex)
+        {
+          Console.WriteLine("Start index should be greater then end index");
+          continue;
+        }
+        foreach (var item in NumberCheckProcessor.ProcessNumbers(startIndex, endIndex))
+        {
+          Console.WriteLine($"{item.Number}, {item.Properties}");
+          
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Press 'C' to continue");
+        if (Console.ReadLine().ToLower() == "c")
+        {
+          continue;
+        }
+        break;
+      }
+
+
     }
   }
 }

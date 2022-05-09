1993bb6531356319b81b0889786cc868fb5de6b6
Stepan_Penyaz <Stepan_Penyaz@epam.com>
2022-05-09 12:31:46 +0400

    Feature: Added test cases for NumberProcessor and NumberCheckService.

---
+++ Unit_7-Kata_1-Odd-Even.Tests/NumberCheckServiceTests.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+
+namespace Unit_7_Kata_1_Odd_Even.Tests
+{
+  public class NumberCheckServiceTests
+  {
+    private readonly NumberCheckService _numberCheckService;
+    [SetUp]
+    public void Setup()
+    {
+      _numberCheckService = new NumberCheckService();
+    }
+
+    [TestCase(3)]
+    [TestCase(5)]
+    [TestCase(7)]
+    [TestCase(9)]
+    [TestCase(11)]
+    [TestCase(29)]
+    [TestCase(33)]
+    [TestCase(59)]
+    public void Check_OddNumber_Returns_Odd()
+    {
+      var result = numberCheckService.CheckNumber(oddNumber);
+
+      Assert.AreEqual(result, numberType.Odd);
+    }
+
+    [TestCase(2)]
+    [TestCase(4)]
+    [TestCase(10)]
+    [TestCase(14)]
+    [TestCase(16)]
+    [TestCase(18)]
+    [TestCase(34)]
+    public void Check_EvenNumber_Returns_Even(int number)
+    {
+      var result = numberCheckService.CheckNumber(number);
+
+      Assert.AreEqual(result, numberType.Even);
+    }
+
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(5)]
+    [TestCase(7)]
+    [TestCase(11)]
+    [TestCase(13)]
+    [TestCase(17)]
+    public void Check_PrimeNumber_Returns_Prime(int number)
+    {
+      var result = numberCheckService.CheckNumber(number);
+
+      Assert.AreEqual(result, numberType.Prime);
+    }
+  }
+}
---
+++ Unit_7-Kata_1-Odd-Even.Tests/NumberProcessorTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Unit_7_Kata_1_Odd_Even.Tests
+{
+  class NumberProcessorTests
+  {
+    [TestCase(1, 4, ExpectedResult = 4)]
+    [TestCase(1, 100, ExpectedResult = 100)]
+    [TestCase(1, 200, ExpectedResult = 200)]
+    public void Process_Numbers_Returns_Correct_Length(int startIndex, int endIndex)
+    {
+      return numberCheckService.ProcessNumbers(startIndex, endIndex).Count;
+    }
+
+    [Test]
+    public void Process_Numbers_1_10_Returns_Correct_Result()
+    {
+      var expected = new List<ProcessedNumber>
+      {
+        {1, "Odd" },
+        {2, "Even, Prime" },
+        {3, "Odd, Prime" },
+        {4, "Even" },
+        {5, "Odd, Prime" },
+        {6, "Even" },
+        {7, "Odd, Prime" },
+        {8, "Even" },
+        {9, "Odd" },
+        {10, "Even" }
+      };
+
+      var result = numberCheckService.ProcessNumbers(1, 10);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Process_Wrong_Numbers_Returns_Result_With_Remarks()
+    {
+      var expected = new List<ProcessedNumber>
+      {
+        {-1, "Incorrect number" },
+        {0, "Incorrect number" },
+      };
+
+      var result = numberCheckService.ProcessNumbers(-1, 0);
+
+      Assert.AreEqual(expected, result);
+    }
+  }
+}
